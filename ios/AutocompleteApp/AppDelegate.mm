8892d249055b04014f90244c01aab88b142863fc
aljary12 <ryadisp@gmail.com>
2024-06-13 03:25:33 +0800

    feat: install react-native-maps

--- ios/AutocompleteApp/AppDelegate.mm
+++ ios/AutocompleteApp/AppDelegate.mm
@@ -1,11 +1,20 @@
 #import "AppDelegate.h"
 
+// React Native Maps -- Google Maps
+#import <GoogleMaps/GoogleMaps.h>
+
+// React Native Config
+#import "RNCConfig.h"
+
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  NSString *googleMapApiKey = [RNCConfig envFor:@"GOOGLE_MAP_API_KEY"];
+  [GMSServices provideAPIKey:googleMapApiKey];
+
   self.moduleName = @"AutocompleteApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
